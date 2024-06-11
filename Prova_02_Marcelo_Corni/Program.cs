471455a687804c7f64661731d341e956ab53a8b1
Marcelo Corni Alves <marcelo@desenvolvedor.net>
2024-06-11 15:19:28 -0300

    Adicionar arquivos de projeto.

---
+++ Prova_02_Marcelo_Corni/Program.cs
@@ -0,0 +1,17 @@
+﻿using Prova_02_Marcelo_Corni;
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Questão 01
+        List<int> lista = new List<int> { 11, 1, 8, 5, 12, 4, 6, 7, 9, 3, 10, 2 };
+        ParticaoSucessiva particao = new ParticaoSucessiva();
+        int mediana = particao.Mediana(lista, 0, lista.Count - 1, lista.Count / 2);
+        Console.WriteLine($"A mediana é: {mediana}");
+
+        Console.ReadLine();
+    }
+}
---
+++ Prova_02_Marcelo_Corni/Questao01.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova_02_Marcelo_Corni
+{
+    public class ParticaoSucessiva
+    {
+        public int Mediana(List<int> lista, int inicio, int fim, int k)
+        {
+            if (inicio <= fim)
+            {
+                int posicaoPivo = Particionar(lista, inicio, fim);
+
+                if (posicaoPivo == k)
+                {
+                    return lista[posicaoPivo];
+                }
+                else if (posicaoPivo > k)
+                {
+                    return Mediana(lista, inicio, posicaoPivo - 1, k);
+                }
+                else
+                {
+                    return Mediana(lista, posicaoPivo + 1, fim, k);
+                }
+            }
+            return int.MinValue;
+        }
+
+        private int Particionar(List<int> lista, int inicio, int fim)
+        {
+            int pivo = lista[inicio];
+            int i = inicio;
+
+            for (int j = inicio + 1; j <= fim; j++)
+            {
+                if (lista[j] < pivo)
+                {
+                    i++;
+                    Trocar(lista, i, j);
+                }
+            }
+            Trocar(lista, inicio, i);
+            return i;
+        }
+
+        private void Trocar(List<int> lista, int i, int j)
+        {
+            int temp = lista[i];
+            lista[i] = lista[j];
+            lista[j] = temp;
+        }
+    }
+
+}
